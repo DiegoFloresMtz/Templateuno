413b7cfb14fc6bfbe5ebab1481d18b323f1586b3
Diego F <diego.flores@n12.mx>
2019-11-28 20:46:14 -0600

    opened in pc

--- templateuno/.cache/sync-requires.js
+++ templateuno/.cache/sync-requires.js
@@ -5,9 +5,9 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\PROBOOK240\\Documents\\GitHub\\Templateuno\\templateuno\\.cache\\dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\PROBOOK240\\Documents\\GitHub\\Templateuno\\templateuno\\src\\pages\\404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\PROBOOK240\\Documents\\GitHub\\Templateuno\\templateuno\\src\\pages\\index.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("C:\\Users\\PROBOOK240\\Documents\\GitHub\\Templateuno\\templateuno\\src\\pages\\page-2.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\windows\\Documents\\N12\\Templateuno\\templateuno\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\windows\\Documents\\N12\\Templateuno\\templateuno\\src\\pages\\404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\windows\\Documents\\N12\\Templateuno\\templateuno\\src\\pages\\index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("C:\\Users\\windows\\Documents\\N12\\Templateuno\\templateuno\\src\\pages\\page-2.js")))
 }
 
